79c6df63575641cfcc6bc6287be505554ff324dc
Alejandro Lizaola <lizaolaa@gmail.com>
2013-01-09 23:57:58 -0600

    Pedidos

---
+++ app/controllers/pedidos_controller.php
@@ -0,0 +1,50 @@
+<?php
+	/**
+	 * 
+	 */
+	class PedidosController extends ApplicationController {
+		
+		 public function reporte($mensaje) {
+    		$this -> pedidos = Pedido::reporte();
+			
+			switch($mensaje){
+				case "eliminado": $this -> mensaje = "El pedido ha sido eliminado correctamente."; break;
+				case "registrado": $this -> mensaje = "El pedido ha sido registrado correctamente."; break;
+			}
+    	 }
+		 public function consulta($id, $mensaje) {
+    		$this -> pedido = Pedido::consultar($id);
+			
+			switch($mensaje){
+				case "modificado": $this -> mensaje = "El pedido ha sido modificado correctamente."; 
+			}
+    	}
+		 public function modificar() {
+			$this -> render(null,null);
+			
+    		$pedido = Pedido::consultar($this -> post("id"));
+			
+			$pedido -> crm_numero = $this -> post("crm_numero");
+			$pedido -> nombre = $this -> post("nombre");
+			$pedido -> total = $this -> post("total");
+			$pedido -> anticipo = $this -> post("anticipo");
+			$pedido -> saldo = $this -> post("saldo");
+			
+			$pedido -> guardar();
+			
+			$this -> redirect("pedidos/consulta/".$pedido -> id."/modificado");
+    	}
+		public function eliminar($id) {
+    		$this -> render(null,null);
+			
+			$pedido = Pedido::consultar($id);
+			
+			$pedido -> eliminar();
+			
+			$this -> redirect("pedidos/reporte/eliminado");
+    	}
+	}	
+
+?>
+
+
