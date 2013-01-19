a0aa86d5570787aab5a20f54de4e4218a1a6188b
Ramiro Vera <raalveco@gmail.com>
2013-01-18 20:04:13 -0600

    Trabajos de Diseñadores

--- app/controllers/disenadores_controller.php
+++ app/controllers/disenadores_controller.php
@@ -62,27 +62,118 @@
 			$this -> redirect("disenadores/reporte/eliminado");
     	}
 		
-		public function trabajo(){
-			$this -> pedidos = Pedido::reporte("estado = 'DISPONIBLE'");
+		public function trabajos(){
+			if(!Session::get("ACCESO")){
+				$this -> redirect("disenadores/index/invalido");
+			}
 			
+			if(Pedido::existe("estado = 'ASIGNADO' AND disenador_id = ".Session::get("id_usuario"))){
+				$this -> redirect("disenadores/pendientes");
+			}
+			
+			$this -> pedidos = Pedido::reporte("estado != 'INACTIVO' AND estado != 'TERMINADO' AND estado != 'NUEVO'");
 		}
 		
+		public function pendientes(){
+			if(!Session::get("ACCESO")){
+				$this -> redirect("disenadores/index/invalido");
+			}
+			
+			if(!Pedido::existe("estado = 'ASIGNADO' AND disenador_id = ".Session::get("id_usuario"))){
+				$this -> redirect("disenadores/trabajos");
+			}
+			
+			$this -> pedidos = Pedido::reporte("estado = 'ASIGNADO' AND disenador_id = ".Session::get("id_usuario"));
+		}
 		
+		public function index($mensaje){
+			Session::set("id_usuario", "");
+			Session::set("usuario", "");
+			Session::set("tipo","");
+			Session::set("ACCESO", false);
+			
+			switch($mensaje){
+				case "error": $this -> mensaje = "El Nombre de Usuario y/o Contraseña son incorrectos, Intente de nuevo."; break;
+				case "invalido": $this -> mensaje = "Acceso Inválido, Por favor Inicie Sesión para poder ingresar a la sección para diseñadores."; break;
+			}
+		}
 		
+		public function ingresar() {
+			$this -> render(null, null);
+			
+			if(Disenador::existe("usuario = '".$this -> post("usuario")."' AND password = '".sha1($this -> post("password"))."'")){
+				$disenador = Disenador::consultar("usuario = '".$this -> post("usuario")."' AND password = '".sha1($this -> post("password"))."'");
+				
+				Session::set("usuario", $this -> post("usuario"));
+				Session::set("tipo","DISEÑADOR");
+				Session::set("ACCESO",true);
+				Session::set("id_usuario", $disenador -> id);
+				
+				if(Pedido::existe("estado = 'ASIGNADO' AND disenador_id = ".Session::get("id_usuario"))){
+					$this -> redirect("disenadores/pendientes");
+				}
+				else{
+					$this -> redirect("disenadores/trabajos");
+				}
+			}
+			else{
+				Session::set("usuario", "");
+				Session::set("tipo","");
+				Session::set("ACCESO", false);
+				
+				$this -> redirect("disenadores/index/error");
+			}   
+    	}
+    	
+    	public function solicitar($id) {
+			$this -> render(null, null);
+			
+			$pedido = Pedido::consultar($id);
+			
+			$pedido -> disenador_id = Session::get("id_usuario");
+			$pedido -> estado = "ASIGNADO";
+			
+			$pedido -> guardar();
+			
+			$this -> redirect("disenadores/pendientes");
+		}
 		
+		public function descargar($ov_cifrada){
+			$this -> render(null, null);
+			
+			$pedido = Pedido::consultar("crm_cifrado = '".$ov_cifrada."'");
+			
+			$pedido -> descargado = $pedido -> descargado + 1;
+			$pedido -> guardar(); 
+			
+			$download = APLICACION_URL ."files/repositorios/originales/".$pedido -> originales;
+			
+			header("location: ".$download);
+			
+			return;
+		}
 		
+		public function trabajo($ov_cifrada, $tipo){
+			$this -> render(null, null);
+			
+			$pedido = Pedido::consultar("crm_cifrado = '".$ov_cifrada."'");
+			
+			if($tipo == "OK"){
+				$this -> redirect("disenadores/disenar/".$ov_cifrada."");
+			}
+			else{
+				$this -> redirect("disenadores/rechazar/".$ov_cifrada."");
+			}
+		}
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		public function cerrar() {
+			$this -> render(null, null);
+			
+			Session::set("usuario", "");
+			Session::set("tipo","");
+			Session::set("ACCESO", false);
+			
+			$this -> redirect("disenadores/index");
+		}
 	}
 ?>
--- app/controllers/uploader_controller.php
+++ app/controllers/uploader_controller.php
@@ -116,10 +116,14 @@
             if($this -> post("caras")==1){
                 $nombre2 = $pedido -> crm_numero." [FYF].zip";
             }
+			
+			$pedido -> originales = $nombre2;
+			$pedido -> descargado = 0;
+			$pedido -> guardar(); 
             
             if(strtoupper($this -> post("tipo_folleto"))=="DA"){
                 rename($directorio."/".$nombre,$directorio."/files/repositorios/listos/".$nombre2);
-				$repositorio = APLICACION_URL."files/repositorioslistos/".$nombre2;
+				$repositorio = APLICACION_URL."files/repositorios/listos/".$nombre2;
             }
             else{
                 rename($directorio."/".$nombre,$directorio."/files/repositorios/originales/".$nombre2);
